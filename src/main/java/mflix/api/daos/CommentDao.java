5b5e007a2572b557230cc36eb3342ea93d37e8f6
Rodrigo Silva <rodrigosilvap93@gmail.com>
2021-01-19 14:05:11 -0300

    Add mostActiveCommenters

--- src/main/java/mflix/api/daos/CommentDao.java
+++ src/main/java/mflix/api/daos/CommentDao.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoWriteException;
 import com.mongodb.ReadConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.*;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
@@ -35,8 +36,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static com.mongodb.client.model.Accumulators.sum;
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Projections.include;
+import static com.mongodb.client.model.Sorts.descending;
 import static com.mongodb.client.model.Updates.set;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -202,12 +208,20 @@ public class CommentDao extends AbstractMFlixDao {
      */
     public List<Critic> mostActiveCommenters() {
         List<Critic> mostActive = new ArrayList<>();
-        // // TODO> Ticket: User Report - execute a command that returns the
-        // // list of 20 users, group by number of comments. Don't forget,
-        // // this report is expected to be produced with an high durability
-        // // guarantee for the returned documents. Once a commenter is in the
-        // // top 20 of users, they become a Critic, so mostActive is composed of
-        // // Critic objects.
+        List<Bson> pipeline = Arrays.asList(group("$email", sum("count", 1)),
+                sort(descending("count")), limit(20));
+
+        MongoCollection<Document> collection = db.getCollection(COMMENT_COLLECTION);
+
+        MongoCursor<Document> iterator = collection.aggregate(pipeline).iterator();
+
+        iterator.forEachRemaining(doc -> {
+            Critic critic = new Critic();
+            critic.setId(doc.getString("_id"));
+            critic.setNumComments(doc.getInteger("count"));
+            mostActive.add(critic);
+        });
+
         return mostActive;
     }
 }
